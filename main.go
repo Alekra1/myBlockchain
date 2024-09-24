c0da8ba3cb22ea220a79a8633cfe7a9624925878
Oleksii <onk230@aubg.bg>
2024-09-24 15:32:05 +0300

    трохи поворкав

--- main.go
+++ main.go
@@ -1,16 +1,39 @@
 ﻿package main
 
-import "time"
+import (
+	"crypto/sha256"
+	"fmt"
+	"time"
+)
 
-type  Block struct {
-	Index int
-	TimeStamp time.Time
-	Data []byte
+type Block struct {
+	Index        int
+	TimeStamp    []byte
+	Data         []byte
 	PreviousHash []byte
-	CurrentHash []byte
+	CurrentHash  []byte
 }
 
-func main(){
+type Blockchain struct {
+	Blocks []*Block
+}
+
+func (block *Block)generateBlock(Data string){
+	block.TimeStamp = []byte(time.Now().Format(time.RFC3339))
+	block.generateHash(block.TimeStamp)
+	block.Data = []byte(Data)
 
 }
 
+func (block *Block) generateHash(TimeStamp []byte) []byte {
+	h := sha256.New()
+	h.Write(TimeStamp)
+	hashBytes := h.Sum(nil)
+	return hashBytes
+}
+
+func main() {
+	block := new(Block)
+	block.CurrentHash = block.generateHash())
+	fmt.Printf("%x", block.CurrentHash)
+}
