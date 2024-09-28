5b15adaed02e031caca6f6624457d0842bee9e9d
alekra1 <alekseyuwww12@gmail.com>
2024-09-28 23:05:35 +0300

    1. added ascii style cool printing text style 2. updated go version

--- main.go
+++ main.go
@@ -3,6 +3,7 @@
 import (
 	"crypto/sha256"
 	"fmt"
+	"github.com/lukesampson/figlet/figletlib"
 	"time"
 )
 
@@ -18,7 +19,7 @@ type Blockchain struct {
 	Blocks []*Block
 }
 
-func (block *Block)generateBlock(Data string){
+func (block *Block) generateBlock(Data string) {
 	block.TimeStamp = []byte(time.Now().Format(time.RFC3339))
 	block.generateHash(block.TimeStamp)
 	block.Data = []byte(Data)
@@ -26,14 +27,25 @@ func (block *Block)generateBlock(Data string){
 }
 
 func (block *Block) generateHash(TimeStamp []byte) []byte {
-	h := sha256.New()
-	h.Write(TimeStamp)
-	hashBytes := h.Sum(nil)
+	hash := sha256.New()
+	hash.Write(TimeStamp)
+	hashBytes := hash.Sum(nil)
 	return hashBytes
+}
+
+func Print(message string){
+	fontsDirectory := "/home/alekra1/myBlockChain/fonts"
+	f, err := figletlib.GetFontByName(fontsDirectory, "smslant")
+	if err != nil {
+		fmt.Println("Could not find that font!")
+		return
+	}
+	figletlib.PrintMsg(message, f, 80, f.Settings(), "left")
 }
 
 func main() {
 	block := new(Block)
-	block.CurrentHash = block.generateHash())
+	block.CurrentHash = block.generateHash([]byte(time.Now().Format(time.RFC3339)))
+	Print("Here is your hash:")
 	fmt.Printf("%x", block.CurrentHash)
 }
